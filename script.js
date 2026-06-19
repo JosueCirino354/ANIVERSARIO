4ebdd8bd29d9c728527d524e75abf1f2902b3480
JosueCirino354 <joguicipe22@gmail.com>
2026-06-19 09:33:49 -0500

    Refactor next function and improve button behavior

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function next(num){
 
+```
 document
 .querySelectorAll('.screen')
 .forEach(screen=>{
 
-screen.classList.remove('active');
+    screen.classList.remove('active');
 
 });
 
@@ -13,30 +14,74 @@ document
 .classList.add('active');
 
 window.scrollTo({
-
-top:0,
-
-behavior:'smooth'
-
+    top:0,
+    behavior:'smooth'
 });
+```
 
 }
 
 const btnNo =
 document.getElementById('btnNo');
 
+if(btnNo){
+
+```
 btnNo.addEventListener('mouseover',()=>{
 
-btnNo.style.position='fixed';
+    btnNo.style.position='fixed';
 
-btnNo.style.left=
-Math.random()*
-(window.innerWidth-120)
-+'px';
+    btnNo.style.left =
+    Math.random() *
+    (window.innerWidth - 120)
+    + 'px';
 
-btnNo.style.top=
-Math.random()*
-(window.innerHeight-80)
-+'px';
+    btnNo.style.top =
+    Math.random() *
+    (window.innerHeight - 80)
+    + 'px';
 
 });
+```
+
+}
+
+const emojis = [
+"🌼",
+"🌸",
+"🌷",
+"💜",
+"🤍"
+];
+
+for(let i=0;i<40;i++){
+
+```
+const flower =
+document.createElement("div");
+
+flower.classList.add("heart");
+
+flower.innerHTML =
+emojis[
+    Math.floor(
+        Math.random()*emojis.length
+    )
+];
+
+flower.style.left =
+Math.random()*100 + "vw";
+
+flower.style.fontSize =
+(Math.random()*20+15)+"px";
+
+flower.style.animationDuration =
+(Math.random()*6+6)+"s";
+
+flower.style.animationDelay =
+(Math.random()*5)+"s";
+
+document.body.appendChild(flower);
+```
+
+}
